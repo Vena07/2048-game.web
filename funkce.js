5fa71a01c2c3e817bbeefd0c63e207b6de8f53b3
Vena07 <vasiknosek@email.cz>
2024-10-18 23:30:33 +0200

    update

--- funkce.js
+++ funkce.js
@@ -1,14 +1,17 @@
 let barvypocet = 1;
 
 function barvy(){
-    let obevenibarev = document.getElementById('paleta');
+    let obevenibarev1 = document.getElementById('paleta');
+    let obevenibarev2 = document.getElementById('buttonpaleta');
     switch(barvypocet){
         case 1:
-            obevenibarev.style.display = 'flex';
+            obevenibarev1.style.display = 'flex';
+            obevenibarev2.style.display = 'none';
             barvypocet = 2;
             break;
         case 2:
-            obevenibarev.style.display = 'none';
+            obevenibarev1.style.display = 'none';
+            obevenibarev2.style.display = 'flex';
             barvypocet = 1;
             break;
     }
